539585e324de0ea26a3821c483d4f6bde5132c1b
Danilo <daniloaugusto0212@hotmail.com>
2021-01-22 21:21:43 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="description" content="Descrição do meu site">
+    <meta charset="keywords" content="palavras, chave, do site">
+    <meta charset="author" content="Danilo Augusto">
+</head>
+<body>
+
+    <header>
+        <div class="container">
+            <div>
+                <div class="logo-1"></div>
+            </div>
+            <div>
+                <div class="logo-2"></div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </header>
+
+    <section class="cover-form">
+        <div class="container bg">
+            <div class="form-container">
+                <h2>Pode acreditarm por dentro é ainda mais bonito.</h2>
+            </div>
+        </div>
+    </section>
+    
+</body>
+</html>
